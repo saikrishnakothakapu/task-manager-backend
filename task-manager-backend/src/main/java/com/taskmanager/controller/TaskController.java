8a90582debba23b95125c1697bbae71833dc49ba
kothakapu saikrishna reddy <saikrishnakothakapu@gmail.com>
2026-04-24 00:30:08 +0530

    feat: updated some minor changes in applications.properties

---
+++ task-manager-backend/src/main/java/com/taskmanager/controller/TaskController.java
@@ -0,0 +1,36 @@
+package com.taskmanager.controller;
+
+import com.taskmanager.dto.CreateTaskRequest;
+import com.taskmanager.dto.TaskResponse;
+import com.taskmanager.exception.UserException;
+import com.taskmanager.repository.TaskRepository;
+import com.taskmanager.repository.UserRepository;
+import com.taskmanager.service.TaskService;
+import com.taskmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/tasks")
+    public ResponseEntity<?> addTask(@RequestBody CreateTaskRequest task, String userEmail) throws UserException {
+
+        TaskResponse  taskResponse = taskService.createTask(task,userEmail);
+        return ResponseEntity.ok(taskResponse);
+    }
+}
